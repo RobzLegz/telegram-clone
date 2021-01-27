0b2fe51dc2d44324b7db7636329f84ff70e95483
Robzlegz <robzlegz@gmail.com>
2021-01-27 09:07:17 +0200

    Initialize project using Create React App

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyB0DqFHDf4C4YhXyYc97hyYbrA0UVGMB08",
+    authDomain: "telegram-clone-8906f.firebaseapp.com",
+    projectId: "telegram-clone-8906f",
+    storageBucket: "telegram-clone-8906f.appspot.com",
+    messagingSenderId: "1094030694715",
+    appId: "1:1094030694715:web:79b329eb22c0527f31ff1e",
+    measurementId: "G-RQVH7PYK59"
+};
