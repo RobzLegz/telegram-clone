7dd54b707bd3cfd168f369d543258b5fc69df6ab
Robzlegz <robzlegz@gmail.com>
2021-01-27 09:53:34 +0200

    styling sidebr

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import "./Sidebar.css";
 import SearchIcon from '@material-ui/icons/Search';
+import BorderColorOutlinedIcon from '@material-ui/icons/BorderColorOutlined';
+import { IconButton } from '@material-ui/core';
 
 function Sidebar() {
     return (
@@ -10,6 +12,9 @@ function Sidebar() {
                     <SearchIcon className="sidebar-search-icon"/>
                     <input className="search-input" placeholder="search"></input>
                 </div>
+                <IconButton variant="outlined" id="sidebar-button">
+                    <BorderColorOutlinedIcon />
+                </IconButton>
                 <div className="sidebar-threads">
                     
                 </div>
