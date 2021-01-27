8adeb2ccda15c74cfd451102da22292981ce3aee
Robzlegz <robzlegz@gmail.com>
2021-01-27 09:16:24 +0200

    setting everything up

---
+++ src/components/Telegram.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./Telegram.css";
+
+function Telegram() {
+    return (
+        <div className="telegram">
+            
+        </div>
+    );
+}
+
+export default Telegram;
