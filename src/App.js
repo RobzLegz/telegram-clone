09b6904478bf4824d94ac4d5c4759019e32c85c5
Robzlegz <robzlegz@gmail.com>
2021-01-27 12:08:33 +0200

    bug fix

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 import Telegram from './components/Telegram';
-
+import { login, selectUser, logout } from './features/userSlice';
+import {useDispatch ,useSelector} from "react-redux";
+import Login from "./components/Login";
+import {auth} from "./firebase";
 
 function App() {
+
+  const user = useSelector(selectUser);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    auth.onAuthStateChanged((authUser) => {
+      if(authUser) {
+        dispatch((
+          login({
+            uid: authUser.uid,
+            phot: authUser.photoURL,
+            email: authUser.email,
+            displayName: authUser.displayName,
+          })
+        ))
+      }else{
+        dispatch(logout());
+      }
+    });
+  }, [dispatch])
+
   return (
     <div className="app">
-      <Telegram />
+      {user ? <Telegram /> : <Login />}
     </div>
   );
 }
--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,5 @@
+import firebase from "firebase";
+
 const firebaseConfig = {
     apiKey: "AIzaSyB0DqFHDf4C4YhXyYc97hyYbrA0UVGMB08",
     authDomain: "telegram-clone-8906f.firebaseapp.com",
@@ -7,3 +9,11 @@ const firebaseConfig = {
     appId: "1:1094030694715:web:79b329eb22c0527f31ff1e",
     measurementId: "G-RQVH7PYK59"
 };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore;
+const auth = firebase.auth();
+const provider = new firebase.auth.GoogleAuthProvider();
+
+export {auth, provider};
+export default db;
